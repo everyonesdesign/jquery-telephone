a6dcd7f561e11cbda6109df377c79f79c1c4cad3
Yura Trambitskiy <tr.yura@yahoo.com>
2014-07-24 01:43:08 +0400

    Configured workspace for the project

---
+++ dist/telephone.js
@@ -0,0 +1,20 @@
+;
+(function ($) {
+
+    $.fn.telephone = function (options) {
+
+        var $this = $(this);
+
+        //setting options
+        var defaults = {
+            responsive: true
+        };
+
+        options = $.extend(defaults, options);
+
+
+    };
+
+
+}(jQuery));
+
